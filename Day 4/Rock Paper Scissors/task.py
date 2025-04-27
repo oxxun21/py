e078539b9872b0a1ed9a172d2e623dc1af1016f6
oxxun21 <ogaeun0121@gmail.com>
2025-04-27 18:37:25 +0900

    list, random, for

--- Day 4/Rock Paper Scissors/task.py
+++ Day 4/Rock Paper Scissors/task.py
@@ -1,3 +1,5 @@
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -24,3 +26,27 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+
+game_images= [rock,paper, scissors]
+
+user_choice = int(input('0 == rock, 1 == paper, 2 == scissors'))
+print(game_images[user_choice])
+
+computer_choice = random.randint(0,2)
+print(f'{computer_choice}')
+print(game_images[computer_choice])
+
+if user_choice >= 3 or user_choice < 0:
+    print('lose')
+elif user_choice == 0 and computer_choice == 2:
+    print('user wins')
+elif computer_choice == 0 and user_choice == 2:
+    print('lose')
+elif computer_choice > user_choice:
+    print('computer wins')
+elif user_choice > computer_choice:
+    print('win')
+elif computer_choice == user_choice:
+    print('draw')
+else:
+    print('error')
